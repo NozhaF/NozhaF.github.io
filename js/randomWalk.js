919375d502c5222f468b4537458c16c570445786
NozhaF <nozha.farjallah@hotmail.com>
2018-09-16 18:21:37 +0100

    made responsive

--- js/randomWalk.js
+++ js/randomWalk.js
@@ -14,16 +14,21 @@ var cspeed = 1;
 var colors = ["#ED1156", "#ED194E", "#ED2247", "#ED2B3F", "#EE3438", "#EE3D31", "#EE4529", "#EF4E22", "#EF571A", "#EF6013", "#F0690C", "#E8720E", "#E17B10", "#D98512", "#D28E14", "#CB9816", "#C3A118", "#BCAA1A", "#B4B41C", "#ADBD1E", "#A6C721", "#96C62F", "#87C53E", "#78C44D", "#69C35C", "#5AC26B", "#4AC17A", "#3BC089", "#2CBF98", "#1DBEA7", "#0EBDB6", "#0EBAB0", "#0EB8AA", "#0EB5A4", "#0EB39E", "#0EB098", "#0EAE92", "#0EAB8C", "#0EA986", "#0EA680", "#0EA47B", "#269376", "#3F8372", "#58736E", "#71626A", "#895266", "#A24262", "#BB315E", "#D4215A"];
 
 
+function windowResized() {
+	resizeCanvas(windowWidth,0.9*windowHeight)
+}
+
+
 
 function setup() {
-  var canvas = createCanvas(windowWidth, 550);
+  var canvas = createCanvas(windowWidth, 0.9*windowHeight);
  canvas.position(0,-15);
   // Move the canvas so it's inside our <div id="sketch-holder">.
   canvas.parent('sketch-holder');
   
   canvas.style('z-index','-1');
 
-  background(255, 1);
+  // background(255, 1);
 
 }
 
